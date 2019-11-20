94ba9ca4cedf3895683803a4efd8718d29bdce31
osogrizz <lhtor@gmail.com>
2019-11-19 17:29:20 -0800

    Adds store, initial state, and carReducer.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { carReducer } from './reducers/carReducer'
+
+const store = createStore(carReducer);
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement);
